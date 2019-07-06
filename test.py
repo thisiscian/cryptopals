0e6267dd8b275722fe1bb7cccce26c480f1891e6
hitcherland <hitcherland@gmail.com>
2019-07-06 15:57:55 +0100

    updating test.py to fix 'outputs' bug

--- test.py
+++ test.py
@@ -23,18 +23,18 @@ test_data = {
     'challenge_3': {
         'inputs': [('1b37373331363f78151b7f2b783431333d78397828372d363c783'
                         '73e783a393b3736')],
-        'outputs': "Cooking MC's like a pound of bacon"
+        'output': "Cooking MC's like a pound of bacon"
     },
     'challenge_4': {
         'inputs': [requests.get('https://cryptopals.com/static/challenge-data/'
                                 '4.txt').text],
-        'outputs': "Now that the party is jumping\n"
+        'output': "Now that the party is jumping\n"
     },
     'challenge_5': {
         'inputs': [("Burning 'em, if you ain't quick and nimble\nI go crazy "
                     "when I hear a cymbal"),
                    'ICE'],
-        'outputs': ('0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2'
+        'output': ('0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2'
                      'a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2'
                      '027630c692b20283165286326302e27282f')
     }
