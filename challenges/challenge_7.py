a4115508a73204c378a20876ef035ab06200c474
hitcherland <hitcherland@gmail.com>
2019-07-08 17:31:18 +0100

    completed challenge 7

--- challenges/challenge_7.py
+++ challenges/challenge_7.py
@@ -3,9 +3,18 @@
 # Set 1: Basics
 # - Challenge #7: AES in ECB mode
 #   see https://cryptopals.com/sets/1/challenges/7
+#
+# this problem breaks my hopes of not using external libraries, as we need to
+# install pycrypto to be able to easily decrypt AES
+#
 
-def main():
-    pass
+import base64
+from Crypto.Cipher import AES
+
+def main(b64_string: str, key: str) -> str:
+    aes_string = base64.b64decode(b64_string)
+    aes = AES.new(key)
+    return aes.decrypt(aes_string).decode()
 
 if __name__ == "__main__":
     main()
