a113f7bdbc4925db76670fc6f0a38224a3d37ea5
hitcherland <hitcherland@gmail.com>
2019-07-08 18:14:51 +0100

    completed challenge 9

--- challenges/challenge_9.py
+++ challenges/challenge_9.py
@@ -4,8 +4,11 @@
 # - Challenge #9: Implement PKCS#7 padding
 #   see https://cryptopals.com/sets/2/challenges/9
 
-def main():
-    pass
+def main(input_string: str, block_size: int) -> str:
+    pad = block_size % len(input_string)
+    padding = bytes([pad] * pad)
+    output = input_string + padding.decode()
+    return output
 
 if __name__ == "__main__":
-    main()
+    main("YELLOW SUBMARINE", 20)
